7da8270c5d6577424fd0d808289138dcc1c9fc34
Missayork <missaelhg2002@gmail.com>
2026-05-12 19:41:30 -0600

    feat: add EF Core infrastructure, DbContext and task repository

--- TaskManager.API/obj/Debug/net9.0/TaskManager.API.AssemblyInfo.cs
+++ TaskManager.API/obj/Debug/net9.0/TaskManager.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+281b6cf00f71cc8ef56d63063625e3d7d66a7db8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4caf26aba9190a3da652e92e08c09c1bf6860ee1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
